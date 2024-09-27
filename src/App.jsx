f875c172046484029a6e54235a9a90a94ca71f82
SarthakJoshi04 <sarthakmanjoshi04@gmail.com>
2024-09-27 10:51:44 +0545

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Note from './component/Notes'
 import './App.css'
+import Footer from './component/Footer'
 
 function App() {  
   return (
     <>
       <Note />
+      <Footer />
     </>
   )
 }
